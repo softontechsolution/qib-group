9ba41ebd6c2b9e852f045228fc16889782343c0e
Alabi Micheal Segun <62448792+Shegzzy@users.noreply.github.com>
2026-04-18 21:57:41 +0100

    strapi added

---
+++ cms/node_modules/@strapi/typescript-utils/lib/generators/content-types/index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const { factory } = require('typescript');
+const { values, pipe, map, sortBy } = require('lodash/fp');
+
+const { models } = require('../common');
+const { emitDefinitions, format, generateSharedExtensionDefinition } = require('../utils');
+
+/**
+ * Generate type definitions for Strapi Content-Types
+ *
+ * @param {object} [options]
+ * @param {object} options.strapi
+ * @param {object} options.logger
+ * @param {string} options.pwd
+ */
+const generateContentTypesDefinitions = async (options = {}) => {
+  const { strapi } = options;
+
+  const { contentTypes } = strapi;
+
+  const contentTypesDefinitions = pipe(
+    values,
+    sortBy('uid'),
+    map((contentType) => ({
+      uid: contentType.uid,
+      definition: models.schema.generateSchemaDefinition(contentType),
+    }))
+  )(contentTypes);
+
+  const formattedSchemasDefinitions = contentTypesDefinitions.reduce((acc, def) => {
+    acc.push(
+      // Definition
+      def.definition,
+
+      // Add a newline between each interface declaration
+      factory.createIdentifier('\n')
+    );
+
+    return acc;
+  }, []);
+
+  const allDefinitions = [
+    // Imports
+    ...models.imports.generateImportDefinition(),
+
+    // Add a newline after the import statement
+    factory.createIdentifier('\n'),
+
+    // Schemas
+    ...formattedSchemasDefinitions,
+
+    // Global
+    generateSharedExtensionDefinition('ContentTypes', contentTypesDefinitions),
+  ];
+
+  const output = emitDefinitions(allDefinitions);
+  const formattedOutput = await format(output);
+
+  return { output: formattedOutput, stats: {} };
+};
+
+module.exports = generateContentTypesDefinitions;
